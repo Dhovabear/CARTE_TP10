aad934d889ea4fee09a0e91b3dc041bd3aa575af
Dhovabear <43112786+dhovabear@users.noreply.github.com>
2019-05-05 21:34:40 +0200

    Ajout du graphique pour les cartes au dessus su tas !
    tout ceci est provisoire puisque la méthode drawTas permettra de dessiner le tas avec la carte au dessus retournée.

--- ProgObjTp/src/TP10/Graphique/CarteDraw.java
+++ ProgObjTp/src/TP10/Graphique/CarteDraw.java
@@ -2,33 +2,147 @@ package TP10.Graphique;
 
 import TP10.Carte;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class CarteDraw {
 
     public static BufferedImage carteRetourner;
+    private static BufferedImage trefle7;
+    private static BufferedImage trefle8;
+    private static BufferedImage trefle9;
+    private static BufferedImage trefle10;
+    private static BufferedImage trefleValet;
+    private static BufferedImage trefleDame;
+    private static BufferedImage trefleRoi;
+    private static BufferedImage trefleAs;
+
+    private static BufferedImage carreau7;
+    private static BufferedImage carreau8;
+    private static BufferedImage carreau9;
+    private static BufferedImage carreau10;
+    private static BufferedImage carreauValet;
+    private static BufferedImage carreauDame;
+    private static BufferedImage carreauRoi;
+    private static BufferedImage carreauAs;
+
+    private static BufferedImage coeur7;
+    private static BufferedImage coeur8;
+    private static BufferedImage coeur9;
+    private static BufferedImage coeur10;
+    private static BufferedImage coeurValet;
+    private static BufferedImage coeurDame;
+    private static BufferedImage coeurRoi;
+    private static BufferedImage coeurAs;
+
+    private static BufferedImage pique7;
+    private static BufferedImage pique8;
+    private static BufferedImage pique9;
+    private static BufferedImage pique10;
+    private static BufferedImage piqueValet;
+    private static BufferedImage piqueDame;
+    private static BufferedImage piqueRoi;
+    private static BufferedImage piqueAs;
 
     public static void loadTextures(){
+        try {
+            carteRetourner = ImageIO.read(CarteDraw.class.getResource("/Cards/cardBack_blue3.png"));
+
+            trefle7 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubs7.png"));
+            trefle8 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubs8.png"));
+            trefle9 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubs9.png"));
+            trefle10 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubs10.png"));
+            trefleValet = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubsJ.png"));
+            trefleDame = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubsQ.png"));
+            trefleRoi = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubsK.png"));
+            trefleAs = ImageIO.read(CarteDraw.class.getResource("/Cards/cardClubsA.png"));
+
+            carreau7 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamonds7.png"));
+            carreau8 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamonds8.png"));
+            carreau9 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamonds9.png"));
+            carreau10 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamonds10.png"));
+            carreauValet = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamondsJ.png"));
+            carreauDame = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamondsQ.png"));
+            carreauRoi = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamondsK.png"));
+            carreauAs = ImageIO.read(CarteDraw.class.getResource("/Cards/cardDiamondsA.png"));
+
+            coeur7 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHearts7.png"));
+            coeur8 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHearts8.png"));
+            coeur9 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHearts9.png"));
+            coeur10 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHearts10.png"));
+            coeurValet = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHeartsJ.png"));
+            coeurDame = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHeartsQ.png"));
+            coeurRoi = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHeartsK.png"));
+            coeurAs = ImageIO.read(CarteDraw.class.getResource("/Cards/cardHeartsA.png"));
+
+            pique7 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpades7.png"));
+            pique8 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpades8.png"));
+            pique9 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpades9.png"));
+            pique10 = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpades10.png"));
+            piqueValet = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpadesJ.png"));
+            piqueDame = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpadesQ.png"));
+            piqueRoi = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpadesK.png"));
+            piqueAs = ImageIO.read(CarteDraw.class.getResource("/Cards/cardSpadesA.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void drawCarteRetourner(JPanel p, int x, int y){
-        Graphics g = p.getGraphics();
+    public static void drawCarteRetourner(Graphics g, JPanel p, int x, int y){
         g.drawImage(carteRetourner,x,y,p);
     }
 
-    public static void drawCarte(JPanel p, int x , int y , Carte c){
-        Graphics g = p.getGraphics();
+    public static void drawCarte(Graphics g , JPanel p, int x , int y , Carte c){
         switch (c.getCouleurCarte()){
             case TREFLE:
-                break;
+                switch (c.getValeurCarte()){
+                    case SEPT: g.drawImage(trefle7,x,y,p); break;
+                    case HUIT: g.drawImage(trefle8,x,y,p); break;
+                    case NEUF: g.drawImage(trefle9,x,y,p); break;
+                    case DIX: g.drawImage(trefle10,x,y,p); break;
+                    case VALET: g.drawImage(trefleValet,x,y,p); break;
+                    case DAME: g.drawImage(trefleDame,x,y,p); break;
+                    case ROI: g.drawImage(trefleRoi,x,y,p); break;
+                    case AS: g.drawImage(trefleAs,x,y,p); break;
+                }
             case CARREAU:
-                break;
+                switch (c.getValeurCarte()){
+                    case SEPT: g.drawImage(carreau7,x,y,p);break;
+                    case HUIT: g.drawImage(carreau8,x,y,p);break;
+                    case NEUF: g.drawImage(carreau9,x,y,p);break;
+                    case DIX: g.drawImage(carreau10,x,y,p);break;
+                    case VALET: g.drawImage(carreauValet,x,y,p);break;
+                    case DAME: g.drawImage(carreauDame,x,y,p);break;
+                    case ROI: g.drawImage(carreauRoi,x,y,p);break;
+                    case AS: g.drawImage(carreauAs,x,y,p);break;
+                }
             case COEUR:
-                break;
+                switch (c.getValeurCarte()){
+                    case SEPT: g.drawImage(coeur7,x,y,p);break;
+                    case HUIT: g.drawImage(coeur8,x,y,p);break;
+                    case NEUF: g.drawImage(coeur9,x,y,p);break;
+                    case DIX: g.drawImage(coeur10,x,y,p);break;
+                    case VALET: g.drawImage(coeurValet,x,y,p);break;
+                    case DAME: g.drawImage(coeurDame,x,y,p);break;
+                    case ROI: g.drawImage(coeurRoi,x,y,p);break;
+                    case AS: g.drawImage(coeurAs,x,y,p);break;
+                }
             case PIQUE:
-                break;
+                switch (c.getValeurCarte()){
+                    case SEPT: g.drawImage(pique7,x,y,p);break;
+                    case HUIT: g.drawImage(pique8,x,y,p);break;
+                    case NEUF: g.drawImage(pique9,x,y,p);break;
+                    case DIX: g.drawImage(pique10,x,y,p);break;
+                    case VALET: g.drawImage(piqueValet,x,y,p);break;
+                    case DAME: g.drawImage(piqueDame,x,y,p);break;
+                    case ROI: g.drawImage(piqueRoi,x,y,p);break;
+                    case AS: g.drawImage(piqueAs,x,y,p);break;
+                }
         }
     }
 }
--- ProgObjTp/src/TP10/mainGraph.java
+++ ProgObjTp/src/TP10/mainGraph.java
@@ -1,16 +1,131 @@
 package TP10;
 
+import TP10.Graphique.CarteDraw;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class mainGraph {
 
     public static JFrame fenetre;
     public static JPanel pan;
+    public static boolean batailleEnCours;
+    public static int gagnant;
+
+    private static Carte j1DessusTas;
+    private static Carte j2DessusTas;
 
     public static void main(String[] args) {
-        fenetre = new JFrame();
-        pan = new JPanel(true);
 
+        batailleEnCours = false;
+        gagnant = 0;
+
+        Paquet pq = Paquet.genererPaquetDe32Cartes();
+        pq.melanger();
+        Paquet piocheJ1 = pq.piocherPlusieursCarte(16);
+        Paquet piocheJ2 = pq.piocherPlusieursCarte(16);
+
+        Paquet tasDevantJ1 = new Paquet();
+        Paquet tasDevantJ2 = new Paquet();
+
+
+
+        System.out.println(piocheJ1);
+        System.out.println(piocheJ2);
+
+
+        fenetre = new JFrame();
+        pan = new JPanel(true){
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.setColor(new Color(50, 100, 27));
+                g.fillRect(0,0,pan.getWidth(),pan.getHeight());
+
+                if(j1DessusTas != null && j2DessusTas != null){
+                    g.setColor(Color.white);
+                    CarteDraw.drawCarte(g,pan,400,50,j1DessusTas);
+                    CarteDraw.drawCarte(g,pan,400,250,j2DessusTas);
+                }else{
+                    g.setColor(Color.green);
+                    g.drawString("Appuyez sur '->' pour commencer !" ,400, 200);
+                }
+            }
+        };
+
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println(e.getKeyCode());
+                if(e.getKeyCode() == 39){
+                    if(gagnant == 0){
+
+                        System.out.println("pioche J1: " + piocheJ1);
+                        System.out.println("pioche J2: " + piocheJ2 + "\n");
+
+                        if(batailleEnCours){
+                            jouerTour(tasDevantJ1, piocheJ1, tasDevantJ2, piocheJ2);
+
+                            updateCardToDraw(tasDevantJ1, tasDevantJ2);
+
+                            if(tasDevantJ1.getCarteAuDessus().estPlusFortQue(tasDevantJ2.getCarteAuDessus())){
+                                System.out.println("J1 remporte la battaille !\n");
+                                batailleEnCours = false;
+                                piocheJ1.fusionnerAvec(tasDevantJ1);
+                                piocheJ1.fusionnerAvec(tasDevantJ2);
+                            }else if(tasDevantJ2.getCarteAuDessus().estPlusFortQue(tasDevantJ1.getCarteAuDessus())){
+                                System.out.println("J2 remporte la battaille !\n");
+                                batailleEnCours = false;
+                                piocheJ2.fusionnerAvec(tasDevantJ2);
+                                piocheJ2.fusionnerAvec(tasDevantJ1);
+                            }else if(tasDevantJ1.getCarteAuDessus().estEgal(tasDevantJ2.getCarteAuDessus())){
+                                System.out.println("Cartes encores egales , la battaille continue!");
+                            }
+
+                        }else{
+                            jouerTour(tasDevantJ1, piocheJ1, tasDevantJ2, piocheJ2);
+
+                            updateCardToDraw(tasDevantJ1, tasDevantJ2);
+
+                            if(tasDevantJ1.getCarteAuDessus().estEgal(tasDevantJ2.getCarteAuDessus())){
+                                batailleEnCours = true;
+                            }else if(tasDevantJ1.getCarteAuDessus().estPlusFortQue(tasDevantJ2.getCarteAuDessus())){
+                                System.out.println("J1 remporte le tour\n");
+                                piocheJ1.fusionnerAvec(tasDevantJ1);
+                                piocheJ1.fusionnerAvec(tasDevantJ2);
+                            }else if(tasDevantJ2.getCarteAuDessus().estPlusFortQue(tasDevantJ1.getCarteAuDessus())){
+                                System.out.println("J2 remporte le tour\n");
+                                piocheJ2.fusionnerAvec(tasDevantJ2);
+                                piocheJ2.fusionnerAvec(tasDevantJ1);
+                            }
+
+                            if(piocheJ1.estVide()){
+                                gagnant = 2;
+                                System.out.println("J1 a perdu !");
+                            }else if(piocheJ2.estVide()){
+                                gagnant = 1;
+                                System.out.println("J2 a perdu !");
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        fenetre.addKeyListener(kl);
+
+
+        CarteDraw.loadTextures();
         fenetre.setContentPane(pan);
         fenetre.setSize(800,600);
         fenetre.setLocationRelativeTo(null);
@@ -25,4 +140,18 @@ public class mainGraph {
 
 
     }
+
+    private static void jouerTour(Paquet tasDevantJ1, Paquet piocheJ1, Paquet tasDevantJ2, Paquet piocheJ2) {
+        tasDevantJ1.ajouterAuDessus(piocheJ1.piocher());
+        System.out.println("J1 pose la carte: " + tasDevantJ1.getCarteAuDessus());
+        tasDevantJ2.ajouterAuDessus(piocheJ2.piocher());
+        System.out.println("J2 pose la carte: " + tasDevantJ2.getCarteAuDessus() + "\n");
+    }
+
+    private static void updateCardToDraw(Paquet tasDevantJ1, Paquet tasDevantJ2) {
+        j1DessusTas = tasDevantJ1.getCarteAuDessus();
+        j2DessusTas = tasDevantJ2.getCarteAuDessus();
+    }
+
+
 }
